ed65b688f5e50e012d587d9327fa3273f982b75a
Prabhakar Sah <pravkrsah@gmail.com>
2025-01-16 19:48:25 -0600

    Commit 1: Added Get and Post method in VillaAPI Controller

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,69 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.villaList);
+        }
+
+        [HttpGet("{id:int}", Name ="GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.Find(x => x.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody]VillaDTO villaDTO)
+        {
+            if (villaDTO is null)
+            {
+                return BadRequest(villaDTO); 
+            }
+            if (villaDTO.Id > 0) 
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(x =>x.Id).FirstOrDefault().Id+1;
+            //villaDTO.Id =VillaStore.villaList.Max(x => x.Id) + 1;
+            //int newVillaId = 
+            //    (
+            //    from v in VillaStore.villaList
+            //    select v.Id
+            //    ).Max() + 1;
+            //VillaStore.villaList.Add(villaDTO);
+            
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+            //return Ok(villaDTO);
+
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_VillaAPI.Models.DTOs;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO> {
+                new VillaDTO { Id = 1, Name = "Beach View"},
+                new VillaDTO { Id = 2, Name = "Pool View"}
+            };
+    }
+}
